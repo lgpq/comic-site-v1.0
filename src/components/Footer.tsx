7ccde50266bbe3904d639557699c8f6b5ea06ca5
lgpq <186059148+lgpq@users.noreply.github.com>
2025-08-22 03:48:43 +0900

    feat: Create common layout components and apply to root

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-auto">
+      <div className="container mx-auto">
+        <p>&copy; {new Date().getFullYear()} COMIC-SITE-V1.0. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
