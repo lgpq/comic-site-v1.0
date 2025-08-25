f7add526d850aa02e3da8bc1b72b1619b3c85e9d
lgpq <186059148+lgpq@users.noreply.github.com>
2025-08-25 09:43:31 +0900

    feat: unify card design2

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export type ComicSeries = {
+  slug: string;
+  title: string;
+  description?: string;
+  thumbnailUrl?: string;
+  lastUpdated?: string; // YYYY-MM-DD
+  firstEpisodeSlug?: string;
+};
+
+export type ComicEpisode = {
+  seriesTitle: string;
+  episodeSlug: string;
+  title: string;
+  date: string; // YYYY-MM-DD
+  tags: string[];
+  pages: string[];
+  thumbnailUrl?: string;
+};
+
+export type Illustration = {
+  url: string;
+  title: string;
+  date: string; // YYYY-MM-DD
+  description?: string;
+};
